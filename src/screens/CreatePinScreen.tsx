dd3b386433c7d0261f28981c1e591c68bd1b2399
Nguyen Duy Tam <duytam.dev@gmail.com>
2022-04-17 12:21:40 +0700

    custom ui CreatePinScreen.tsx

--- src/screens/CreatePinScreen.tsx
+++ src/screens/CreatePinScreen.tsx
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import * as ImagePicker from 'expo-image-picker';
 import {
   Text, View, StyleSheet, Image, Button, TextInput, Alert,
+  TouchableOpacity,
 } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation } from '@react-navigation/native';
 import { onSubmitPin } from '../services/ProductService';
-import CONSTANTS from '../constants';
+import CONSTANTS, { COLORS } from '../constants';
+import MyButton from '../components/AuthScreen/MyButton';
 
 // eslint-disable-next-line no-redeclare
 interface ImagePicker{
@@ -91,44 +93,80 @@ const CreatePinScreen = () => {
   };
   return (
         <View style={styles.container}>
-            <Text>Hello world</Text>
-            <Button title={'Select image'} onPress={pickImage}/>
-            {
-                imagePicker
-                && <>
-                  <Image style={styles.image} source={{ uri: imagePicker.uri }}/>
-                  <View style={styles.textInput}>
-                    <TextInput value={title} onChangeText={(value) => setTitle(value)} placeholder={'Title'}/>
-                  </View>
-                  <View style={styles.textInput}>
-                    <TextInput value={description} onChangeText={(value) => setDescription(value)} placeholder={'Description'}/>
-                  </View>
-                  <Button title={'Submit pin'} onPress={handleSubmitPin}/>
-                </>
-            }
+            {/* <Button title={'Select image'} onPress={pickImage}/> */}
+            {/* { */}
+            {/*    imagePicker */}
+            {/*    && <> */}
+            {/*      <Image style={styles.image} source={{ uri: imagePicker.uri }}/> */}
+            {/*      <View style={styles.textInput}> */}
+            {/*        <TextInput value={title} onChangeText={(value) => setTitle(value)} placeholder={'Title'}/> */}
+            {/*      </View> */}
+            {/*      <View style={styles.textInput}> */}
+            {/*        <TextInput value={description} onChangeText={(value) => setDescription(value)} placeholder={'Description'}/> */}
+            {/*      </View> */}
+            {/*      <Button title={'Submit pin'} onPress={handleSubmitPin}/> */}
+            {/*    </> */}
+            {/* } */}
+          <View style={styles.imagePickerContainer}>
+            <Image style={styles.image} source={{ uri: imagePicker ? imagePicker.uri : 'https://c03uk1vc.cloudimg.io/width/600/q60/https://static.wapmaker.net/5bc74209fce1810870a2ca73/2/trang%20phong%20thuy.jpg' }}/>
+            <TouchableOpacity onPress={pickImage} style={styles.btnPickImage}>
+              <Text numberOfLines={2} style={styles.textPickImage}>{imagePicker ? 'Chọn ảnh khác' : 'Lựa chọn\n hình ảnh'}</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.formContainer}>
+              <View style={styles.inputContainer}>
+                <Text>Tiêu đề</Text>
+                  <TextInput value={title} onChangeText={(value) => setTitle(value)} placeholder={'Đặt tiêu đề cho hình ảnh của bạn'} style={styles.textInput}/>
+              </View>
+            <View style={styles.inputContainer}>
+              <Text>Mô tả</Text>
+              <TextInput multiline={true} value={description} onChangeText={(value) => setDescription(value)} placeholder={'Đặt mô tả cho hình ảnh của bạn'} style={styles.textInput}/>
+            </View>
+            <MyButton style={styles.btnSubmit} title={'Thêm'} onPress={handleSubmitPin}/>
+          </View>
 
         </View>
   );
 };
 
 const styles = StyleSheet.create({
-  textInput: {
-    width: '100%',
-    height: 48,
-    borderRadius: 4,
-    borderWidth: 1,
-    justifyContent: 'center',
-    paddingHorizontal: 8,
-    marginVertical: 8,
-    borderColor: '#000',
+  btnSubmit: {
+    marginTop: 20,
+    borderRadius: 24,
+  },
+  inputContainer: {
+    marginTop: 8,
+  },
+  formContainer: {
+    marginTop: 8,
+  },
+  textPickImage: {
+    fontSize: 12,
+    color: COLORS.primary,
+  },
+  btnPickImage: {
+    alignItems: 'center',
+    flexGrow: 1,
+    marginLeft: 8,
   },
   image: {
-    width: 450,
-    aspectRatio: 1,
+    width: '72%',
+    aspectRatio: 3 / 4,
+    borderRadius: 8,
+  },
+  imagePickerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  textInput: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    width: '100%',
+    justifyContent: 'center',
   },
   container: {
     flex: 1,
-    padding: 16,
+    paddingTop: 20,
     backgroundColor: '#fff',
   },
 });
